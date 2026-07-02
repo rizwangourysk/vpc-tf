148831414b5a29799fe5bf6d4a53e6520db65ad7
rizwangourysk <rizwangourysk@gmail.com>
2026-07-02 11:28:50 +0530

    added outputs

--- output.tf
+++ output.tf
@@ -1,1 +1,34 @@
-#
+# count use
+#output "ec2_public_ip" {
+# value = aws_instance.my_instance[*].public_ip
+
+#}
+
+#output "ec2_public_dns" {
+# value = aws_instance.my_instance[*].public_dns
+#}
+
+#output "ec2_private_ip" {
+# value = aws_instance.my_instance[*].private_ip
+#}
+
+
+# for_each use
+output "ec2_public_ip" {
+  value = [
+    for instance in aws_instance.my_instance : instance.public_ip
+  ]
+
+}
+output "ec2_public_dns" {
+  value = [
+    for instance in aws_instance.my_instance : instance.public_dns
+  ]
+
+}
+output "ec2_private_ip" {
+  value = [
+    for instance in aws_instance.my_instance : instance.private_ip
+  ]
+
+}
